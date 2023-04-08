e1cb4ff9cc6d1b7996d3f0bcd3abe2ae65693468
JarekChmielewski <j.chmielewski82@gmail.com>
2023-04-08 15:06:29 +0200

    methods

---
+++ 03_methods_and_functions/methods.py
@@ -0,0 +1,11 @@
+# methods perform specific actions on an object and can also take arguments object.method(arg1, arg2 ..)
+
+#create a simple list
+lst = [1, 2, 3, 4, 5, 2, 2]
+
+lst.append(6)
+print(lst)
+
+# count the number of occurrences of element in the list
+print(lst.count(2))
+
